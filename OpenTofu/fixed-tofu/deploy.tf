944cd915d09739acebbb9049673b1ab148024182
Jordan <jordan@forrest.one>
2025-04-29 01:00:39 +0100

    Added Additional Ingress for EC2

--- OpenTofu/fixed-tofu/deploy.tf
+++ OpenTofu/fixed-tofu/deploy.tf
@@ -156,17 +156,25 @@ resource "aws_security_group" "ec2_sg" {
   vpc_id = aws_vpc.main.id
 
   ingress {
-    from_port   = 22
-    to_port     = 22
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+    description = "HTTP"
   }
-
   ingress {
-    from_port   = 6443
-    to_port     = 6443
+    from_port   = 443
+    to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+    description = "HTTPS"
+  }
+  ingress {
+    from_port   = 61616
+    to_port     = 61616
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "ONMS-ActiveMQ
   }
 
   egress {
